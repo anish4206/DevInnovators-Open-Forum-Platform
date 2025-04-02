fff2f0abc5877810291e137aee481bcea124ce94
vaibhav09981 <vaibhavjj99@gmail.com>
2025-04-02 10:44:22 +0530

    NAVBAR CREATED AND DARK MODE LIGHT MODE FIXED

--- server.js
+++ server.js
@@ -10,15 +10,23 @@ app.use(express.urlencoded({ extended: true })); // Parse form data
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "views"))); // Serve HTML files from views directory
+
+// Explicitly serve partials 
+app.use('/partials', express.static(path.join(__dirname, 'views/partials')));
 
 // Import and use the main router
-const router = require("./routes/router");
-app.use("/api", router);  // All API routes will be prefixed with /api
+// const router = require("./routes/router");
+// app.use("/api", router);  // All API routes will be prefixed with /api
 
 // Serve Home Page (HTML)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// Serve Login Page
+app.get("/auth/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "auth", "login.html"));
 });
 
 const PORT = process.env.PORT || 3000;
